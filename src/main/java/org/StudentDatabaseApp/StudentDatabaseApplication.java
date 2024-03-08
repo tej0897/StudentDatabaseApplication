fa761bd7e5ce27268eb0d55705deb68ad80cebe1
tej0897 <tej897@gmail.com>
2024-03-08 21:35:09 +0530

    last commit for 8th March

--- src/main/java/org/StudentDatabaseApp/StudentDatabaseApplication.java
+++ src/main/java/org/StudentDatabaseApp/StudentDatabaseApplication.java
@@ -1,5 +1,9 @@
+package org.StudentDatabaseApp;
+
 public class StudentDatabaseApplication {
     public static void main(String[] args) {
-        
+        // ask how many students we want to add
+
+        // create "n" number of new students
     }
 }
