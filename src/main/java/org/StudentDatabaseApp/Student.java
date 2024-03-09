741a4c2c4ae95859f71f6c33b22bff6583ca5b55
tej0897 <tej897@gmail.com>
2024-03-09 21:11:30 +0530

    save-1 9th march

--- src/main/java/org/StudentDatabaseApp/Student.java
+++ src/main/java/org/StudentDatabaseApp/Student.java
@@ -1,5 +1,26 @@
 package org.StudentDatabaseApp;
 
 public class Student {
-        // constructor prompts user to enter student's name and year
+
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private final int costOfCourse = 600;
+
+
+
+    // constructor prompts user to enter student's name and year
+
+    // generate an ID
+
+    // Enroll in courses
+
+    // view Balance
+
+    // pay Tuition
+
+    // Show Status
 }
